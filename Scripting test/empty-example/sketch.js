73f8b391a3a7258de41989554d7efefe386e7819
SwhUniAcc <samuel2.williams-hicks@live.uwe.ac.uk>
2025-10-10 14:01:13 +0100

    Initial commit

---
+++ Scripting test/empty-example/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+createCanvas(1920, 1080);
+}
+
+function draw() {
+background("Black");
+circle(400,400,200);
+}
+
+function draw() {
+if (mouseIsPressed === true) {
+    fill(0, 71, 171);
+} else {
+    fill(0);
+}
+
+circle(mouseX, mouseY, 100);
+
+}
