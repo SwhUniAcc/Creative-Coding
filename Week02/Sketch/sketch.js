c7a06fc3ee112724d3c5a10ca8d293286954c460
SwhUniAcc <samuel2.williams-hicks@live.uwe.ac.uk>
2025-11-07 14:48:52 +0000

    Finished Week 2

--- Week02/Sketch/sketch.js
+++ Week02/Sketch/sketch.js
@@ -1,55 +1,77 @@
-let rectWidth = 350;
+let rectWidth = 360;
 let rectHeight = 125;
 let ellHeight = 360;
 let ellWidth = 360;
 
 // creating a background
 function setup() {
-    createCanvas(1880, 990)
+    createCanvas(720, 720)
     background(255);
     noLoop();
 }
 
 // display stuff on the background
 function draw() {
-// Text 1    
-    textSize(25);
-    text("Test One", 5, 25); 
+     noStroke();
 
-// Text 2
-    textSize(25);
-    fill(0, 75, 255);
-    stroke(10);
-    strokeWeight(5);
-    text("Test Two", 5, 50);
+// Body
+    fill(0, 71, 171);
+    rect(150, 500, 420, 300, 20);
 
-// Ellipse 1
-    fill(75, 0, 225);
-    ellipse(940, 495, ellWidth, ellHeight);
-    console.log(ellWidth);
-    console.log(ellHeight);
+// Arm Shadow L
+    fill(0, 61, 161);
+    rect(225, 550, 20, 300, 5);
 
-// Retangale 1
-    fill(75, 255, 0);
-    rect(765, 150, rectWidth, rectHeight);
-    console.log(rectWidth);
-    console.log(rectHeight);
+// Arm Shadow R
+    fill(0, 61, 161);
+    rect(475, 550, 20, 300, 5);
 
-// Retangale 2 (Halved)
-    fill(255, 75, 0);
-    rect(765, 75, rectWidth/2, rectHeight/2)
-    console.log(rectWidth/2);
-    console.log(rectHeight/2);
+// Hood
+    fill(0, 61, 161);
+    ellipse(360, 475, ellWidth, ellHeight/2);
 
- //Retangle 3 (Randomized)
-    noFill()
-    drawShape(rectWidth, rectHeight)
-}
+// Head
+    fill(255, 228, 173);
+    ellipse(360, 360, ellWidth, ellHeight);
 
-function drawShape(rectangleWidth, rectangleHeight) {
-    let xPos = random(0, rectWidth);
-    let yPos = random(0, rectHeight);
+// Eye L
+    fill(255);
+    ellipse(300, 360, 70, 30);
 
-    rect(xPos, yPos, rectangleWidth, rectangleHeight);
+// Iris L
+    fill(173, 216, 255);
+    ellipse(300, 360, 32, 30);
+
+ // Pupil L
+    fill(0);
+    ellipse(300, 360, 21, 20);
+
+// Eye R
+    fill(255);
+    ellipse(425, 360, 70, 30);
+
+// Iris r
+    fill(173, 216, 255);
+    ellipse(425, 360, 32, 30);
+
+ // Pupil R
+    fill(0);
+    ellipse(425, 360, 21, 20);
+
+// Hair Mid
+    fill(28, 23, 19);
+    rect(170, 180, 380, 75, 20);
+
+ // Hair L
+    fill(28, 23, 19);
+    rect(170, 225, 75, 275, 20);
+
+// Hair R
+    fill(28, 23, 19);
+    rect(475, 225, 75, 275, 20);
+
+// Mouth
+    fill(0);
+    rect(325, 475, 75, 10, 5);
 
 }
